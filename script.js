f7d9ba6f2bba8e793a13535b495bc5a8a27b65c5
baraatharwat <baraatharwat171@gmail.com>
2026-06-11 10:33:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let cart = [];
 
-function addItem(item) {
+window.addItem = function(item) {
     cart.push(item);
     document.getElementById("cart").innerHTML =
         "🛒 Cart (" + cart.length + ")";
