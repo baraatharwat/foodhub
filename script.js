be16e721046594997bad2c73f35835639bb63b25
baraatharwat <baraatharwat171@gmail.com>
2026-06-11 10:40:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ let cart = [];
 
 window.addItem = function(item) {
     cart.push(item);
+    updateCart();
+}
+
+window.removeItem = function(item) {
+    let index = cart.indexOf(item);
+    if (index > -1) {
+        cart.splice(index, 1);
+    }
+    updateCart();
+}
+
+function updateCart() {
     document.getElementById("cart").innerHTML =
         "🛒 Cart (" + cart.length + ")";
 }
